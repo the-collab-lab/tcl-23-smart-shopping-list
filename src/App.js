d9281fde2c094eea0308f3db16c511b5be1a6b9a
jamesncox <jamesncox@yahoo.com>
2021-04-05 17:55:34 -0500

    Remove unneccessary header from App.js

--- src/App.js
+++ src/App.js
@@ -9,22 +9,8 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <List />
-          <AddItem />
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <List />
+        <AddItem />
       </div>
     </Router>
   );
