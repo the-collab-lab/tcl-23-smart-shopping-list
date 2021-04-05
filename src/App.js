4cfad1f71ba429df4635399b91c89f5a1d037cdd
jamesncox <jamesncox@yahoo.com>
2021-04-05 17:51:29 -0500

    Import Router, Switch and Route from react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,27 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 import List from './pages/List';
 import AddItem from './pages/AddItem';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <List />
-        <AddItem />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <List />
+          <AddItem />
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Router>
   );
 }
 
