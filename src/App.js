e99898f2a05a1dac510cec835769d94cbaddd33c
Zenzi Ali <zeeathome935@gmail.com>
2021-04-10 12:08:14 -0400

    improved readibility by removing commented uneccesary code

--- src/App.js
+++ src/App.js
@@ -48,40 +48,4 @@ const App = () => {
   );
 };
 
-// function App() {
-
-// }
-
 export default App;
-
-/*
-const [list, setList] = React.useState([]); 
-  React.useEffect(() => {
-    const fetchData = async () => {
-      const db = firebase.firestore()
-      const data = await db.collection("list").get()
-      setData(data.map(doc => doc.data()))
-    }//TODO: REPLACE WORD LIST WITH DATA ACCESS POINT
-    
-  }, [])
-  */
-
-//  return (
-//   <div className="App">
-//     <header className="App-header">
-//       <img src={logo} className="App-logo" alt="logo" />
-//       <p>
-//         Edit <code>src/App.js</code> and save to reload.
-//       </p>
-//       <a
-//         className="App-link"
-//         href="https://reactjs.org"
-//         target="_blank"
-//         rel="noopener noreferrer"
-//       >
-//         Learn React
-//       </a>
-//     </header>
-//   </div>
-// );
-// };
