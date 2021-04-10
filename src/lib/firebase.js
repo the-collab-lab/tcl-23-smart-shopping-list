4d1643cce95f6be62cd6ad1c783a302470f2602c
jssckbl <jessicawoudsma@gmail.com>
2021-04-10 17:16:34 -0500

    removed commented code out of firebase.js file

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -20,17 +20,3 @@ const firebaseInstance = firebase.initializeApp(firebaseConfig);
 let db = firebaseInstance.firestore();
 
 export { db };
-
-// firebase.initializeApp({
-//   apiKey: 'AIzaSyB-RVe8Icisr03G002oOc9Un7GXFjIKe-M',
-//   authDomain: 'tcl-23-shopping-list.firebaseapp.com',
-//   databaseURL: 'https://tcl-23-shopping-list-default-rtdb.firebaseio.com',
-//   projectId: 'tcl-23-shopping-list',
-//   storageBucket: 'tcl-23-shopping-list.appspot.com',
-//   messagingSenderId: '17231198664',
-//   appId: '1:17231198664:web:180626daef3992d3cfe8fd',
-// });
-
-// let db = firebase.firestore();
-
-// export { db };
