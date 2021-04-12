4a862774690e1a3868c9f479e14f3dbd331e3883
Ana Rocha <apdsrocha@gmail.com>
2021-04-12 19:28:43 -0300

    add onclick function to add token and redirect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,16 @@ export default function Home() {
   const isThereToken = checkLocalStorage('token', '');
   const history = useHistory();
 
+  function handleClick() {
+    addLocalStorage('token');
+    history.push('/list');
+  }
+
   return (
     <div>
       {isThereToken ? history.push('/list') : ''}
       <h1>Welcome screen</h1>
-      <button>Add List</button>
+      <button onClick={handleClick}>Add List</button>
     </div>
   );
 }
