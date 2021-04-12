10d838d30a51709ecba8c6dadb2d145bd6a6dc4d
Ana Rocha <apdsrocha@gmail.com>
2021-04-12 19:09:06 -0300

    import token and function 'addLocalStorage'

--- src/lib/localStorage.js
+++ src/lib/localStorage.js
@@ -1,5 +1,11 @@
+import getToken from './tokens';
+
 export function checkLocalStorage(key, value) {
   const saveToken = window.localStorage.getItem(key);
   console.log(saveToken);
   return saveToken ? JSON.parse(saveToken) : value;
+}
+
+export function addLocalStorage(key) {
+  window.localStorage.setItem(key, JSON.stringify(getToken()));
 }
