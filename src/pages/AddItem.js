4158cc25cbc639f5f151978aacfeb7dc1a7df2b2
Zenzi Ali <zeeathome935@gmail.com>
2021-04-12 15:55:54 -0400

    created preliminary datamodel for firebase form

--- src/pages/AddItem.js
+++ src/pages/AddItem.js
@@ -1,3 +1,19 @@
+import { db } from './lib/firebase';
+import { useCollection } from 'react-firebase-hooks/firestore';
+
 export default function AddItem(props) {
+  const createListItem = () => {
+    db.collection('list')
+      .doc('this_weeks_list')
+      .collection('shopping_items')
+      .add({ item_name: 'banana', purchase_frequency: 7, last_purchased: null })
+      .then((documentReference) => {
+        console.log('document reference ID', documentReference.id);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
   return <h1>This is the form</h1>;
 }
