9ae5834718730adbb642116754f62efc1c9d183c
jamesncox <jamesncox@yahoo.com>
2021-04-12 15:16:11 -0500

    Add form to AddItem.js

--- src/pages/AddItem.js
+++ src/pages/AddItem.js
@@ -1,4 +1,4 @@
-import { db } from './lib/firebase';
+import { db } from '../lib/firebase';
 import { useCollection } from 'react-firebase-hooks/firestore';
 
 export default function AddItem(props) {
@@ -15,5 +15,25 @@ export default function AddItem(props) {
       });
   };
 
-  return <h1>This is the form</h1>;
+  return (
+    <>
+      <h1>Add Item</h1>
+      <form>
+        <label>
+          Item Name
+          <input type="text" name="name" placeholder="Item Name" />
+        </label>
+        <br />
+        <label>
+          Purchase Frequency
+          <select>
+            <option value={7}>7 Days</option>
+            <option value={14}>14 Days</option>
+            <option value={30}>30 Days</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </>
+  );
 }
