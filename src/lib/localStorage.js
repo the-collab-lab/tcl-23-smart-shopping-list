8217ba17a9a9a93811ddd703530c6556e8d04d33
Ana Rocha <apdsrocha@gmail.com>
2021-04-12 19:26:29 -0300

    remove console.log

--- src/lib/localStorage.js
+++ src/lib/localStorage.js
@@ -2,7 +2,6 @@ import getToken from './tokens';
 
 export function checkLocalStorage(key, value) {
   const saveToken = window.localStorage.getItem(key);
-  console.log(saveToken);
   return saveToken ? JSON.parse(saveToken) : value;
 }
 
