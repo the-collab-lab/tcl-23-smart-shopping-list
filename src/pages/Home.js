718e93c81d7107df774f920c0e7a3722251e7e63
Ana Rocha <apdsrocha@gmail.com>
2021-04-12 19:20:18 -0300

    import useHistory and redirect if token is found

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
 import { checkLocalStorage, addLocalStorage } from '../lib/localStorage';
+import { useHistory } from 'react-router-dom';
 
 export default function Home() {
+  const isThereToken = checkLocalStorage('token', '');
+  const history = useHistory();
+
   return (
     <div>
+      {isThereToken ? history.push('/list') : ''}
       <h1>Welcome screen</h1>
       <button>Add List</button>
     </div>
