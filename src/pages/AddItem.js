524d6e0eb7ea0a04d1835b01867bc4749923542f
Ana Rocha <apdsrocha@gmail.com>
2021-04-12 18:23:51 -0300

    bring NavBar into List and Add Item view

--- src/pages/AddItem.js
+++ src/pages/AddItem.js
@@ -1,3 +1,10 @@
+import NavBar from '../components/NavBar';
+
 export default function AddItem(props) {
-  return <h1>This is the form</h1>;
+  return (
+    <>
+      <h1>This is the form</h1>
+      <NavBar />
+    </>
+  );
 }
--- src/pages/List.js
+++ src/pages/List.js
@@ -1,3 +1,10 @@
+import NavBar from '../components/NavBar';
+
 export default function List(props) {
-  return <h1>THIS IS THE LIST</h1>;
+  return (
+    <>
+      <h1>THIS IS THE LIST</h1>
+      <NavBar />
+    </>
+  );
 }
