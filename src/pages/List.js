de1561b231b68d048983ac5a6c8efe1c52e0cccd
Ana Rocha <apdsrocha@gmail.com>
2021-04-15 17:51:46 -0300

    checks for token, redirects to Home in case of falsy value

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,8 +1,13 @@
 import NavBar from '../components/NavBar';
+import { checkLocalStorageForKey } from '../lib/localStorage';
+import { useHistory } from 'react-router-dom';
 
 export default function List(props) {
+  const isThereToken = checkLocalStorageForKey('token', '');
+  const history = useHistory();
   return (
     <>
+      {isThereToken ? '' : history.push('/')}
       <h1>THIS IS THE LIST</h1>
       <NavBar />
     </>
