74cd92e99bc374e3a814185cce2e5dab475e690b
jssckbl <jessicawoudsma@gmail.com>
2021-04-17 18:29:30 -0500

    moved Redirect to App.js where routes are

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,9 +1,6 @@
-import { Redirect } from 'react-router-dom';
-
 export default function List(props) {
   return (
     <>
-      {props.token ? null : <Redirect to="/" />}
       <h1>THIS IS THE LIST</h1>
     </>
   );
