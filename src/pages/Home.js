a6ac8d9b0c410d8d0aeb1e7d68fedabe883e6c05
Ana Rocha <apdsrocha@gmail.com>
2021-04-17 18:18:02 -0300

    Alters function handleClick to generate token

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,20 +2,23 @@ import {
   checkLocalStorageForKey,
   addKeyValuePairToLocalStorage,
 } from '../lib/localStorage';
+import getToken from '../lib/tokens';
 import { useHistory } from 'react-router-dom';
 
-export default function Home() {
-  const isThereToken = checkLocalStorageForKey('token', '');
+export default function Home(props) {
+  const retrievedToken = checkLocalStorageForKey('token', '');
   const history = useHistory();
 
   function handleClick() {
-    addKeyValuePairToLocalStorage('token');
+    const token = getToken();
+    addKeyValuePairToLocalStorage('token', token);
+    props.setToken(token);
     history.push('/list');
   }
 
   return (
     <main>
-      {isThereToken ? history.push('/list') : ''}
+      {retrievedToken ? history.push('/list') : ''}
       <h1>Welcome screen</h1>
       <button onClick={handleClick}>Add List</button>
     </main>
