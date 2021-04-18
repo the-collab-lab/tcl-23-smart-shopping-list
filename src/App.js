8f199f4ee0e1f0655fb1370d43fe9776422c25b1
jssckbl <jessicawoudsma@gmail.com>
2021-04-18 09:19:04 -0500

    updated code format when checking for token

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ const App = () => {
             <AddItem />
           </Route>
         </Switch>
-        {token ? <NavBar /> : null}
+        {token && <NavBar />}
 
         <div>
           <h1>Grocery List</h1>
