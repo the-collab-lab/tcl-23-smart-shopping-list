276cd8bafbfdc6881401677c3720fb143a0ec4c6
jamesncox <jamesncox@yahoo.com>
2021-04-18 12:26:50 -0500

    Remove setLastPurchased(null) from createListItem because React error

--- src/pages/AddItem.js
+++ src/pages/AddItem.js
@@ -29,7 +29,7 @@ export default function AddItem(props) {
         console.log(error.message);
       });
     setItemName('');
-    setPurchaseFrequency(null);
+    // setPurchaseFrequency(null);
     setLastPurchased(null);
   };
 
