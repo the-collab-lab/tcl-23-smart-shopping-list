6c8692c3563e59f4bd7e559451818760dda9f209
jssckbl <jessicawoudsma@gmail.com>
2021-04-21 15:20:33 -0500

    changed useState to be null as default to 7

--- src/pages/AddItem.js
+++ src/pages/AddItem.js
@@ -5,7 +5,7 @@ import { useHistory } from 'react-router-dom';
 
 export default function AddItem(props) {
   const [itemName, setItemName] = useState('');
-  const [purchaseFrequency, setPurchaseFrequency] = useState(null);
+  const [purchaseFrequency, setPurchaseFrequency] = useState(7);
   const [lastPurchased, setLastPurchased] = useState(null);
   const [listItems, setListItems] = useState([]);
 
