697ae92cc1122817ca8f39949c58152a52c85d5f
jamesncox <jamesncox@yahoo.com>
2021-05-16 20:44:39 -0500

    Update alphabetizeListItems with comment about capital words being sorted first

--- src/pages/List.js
+++ src/pages/List.js
@@ -88,6 +88,7 @@ export default function List({ token }) {
   }
 
   const alphabetizeListItems = (list) => {
+    // logic in here isn't checking for capitalization
     const sortedList = list.sort((a, b) => {
       if (a.data().item_name < b.data().item_name) {
         return -1;
