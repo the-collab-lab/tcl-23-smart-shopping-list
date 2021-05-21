cf2a303e5a6c0a9d6dfed4abb0aa4c4ce52c3917
jamesncox <jamesncox@yahoo.com>
2021-05-21 10:20:07 -0500

    Add custom color palette to theme: extend: colors:

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'paradise-pink': 'EF476F',
+        'orange-yellow': 'FFD166',
+        'caribbean-green': '06D6A0',
+        'blue-ncs': '118AB2',
+        'midnight-green': '073B4C',
+      },
+    },
   },
   variants: {
     extend: {},
