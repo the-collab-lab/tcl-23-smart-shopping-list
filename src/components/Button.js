54df6654a98e262ab3be3d7b56a39ca0d44be8af
jamesncox <jamesncox@yahoo.com>
2021-05-23 15:35:14 -0500

    Revert back to hover:bg-caribbean-green

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ export default function Button({ onClick, tailwind, text }) {
   return (
     <button
       onClick={onClick}
-      className={`w-full bg-orange-yellow text-midnight-green rounded p-2 text-lg font-medium hover:bg-blue-ncs ${tailwind}`}
+      className={`w-full bg-orange-yellow text-midnight-green rounded p-2 text-lg font-medium hover:bg-caribbean-green ${tailwind}`}
     >
       {text}
     </button>
