6bd7149cfd79c4d5f00658bc26be33a8ab7ea41a
jamesncox <jamesncox@yahoo.com>
2021-05-23 15:30:47 -0500

    Change hover:bg-caribbean-green to hover:bg-blue-ncs

--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -2,7 +2,7 @@ export default function IconButton({ onClick, tailwind, text }) {
   return (
     <button
       onClick={onClick}
-      className={`bg-gray-200 text-midnight-green px-2 rounded font-medium py-0 ml-2 hover:bg-caribbean-green ${tailwind}`}
+      className={`bg-gray-200 text-midnight-green px-2 rounded font-medium py-0 ml-2 hover:bg-blue-ncs ${tailwind}`}
     >
       {text}
     </button>
