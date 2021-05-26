09e8a6689c3b80e7a80161a241bf82813d64ea6f
jssckbl <jessicawoudsma@gmail.com>
2021-05-26 15:59:06 -0500

    added trashbin hero icon to delete function for grocery list items

--- src/pages/List.js
+++ src/pages/List.js
@@ -212,7 +212,20 @@ export default function List({ token }) {
         />
         <label htmlFor={doc.id}>{doc.data().item_name}</label>
         <button key={doc.id} onClick={() => deleteItem(doc.id)}>
-          Delete
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-8 w-8"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="white"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+            />
+          </svg>
         </button>
       </li>
     );
