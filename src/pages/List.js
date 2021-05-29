d5534fbf4e9066884341232d07cfe8e9903921c0
Ana Rocha <apdsrocha@gmail.com>
2021-05-29 15:35:05 -0300

    PR fixes

--- src/pages/List.js
+++ src/pages/List.js
@@ -273,22 +273,22 @@ export default function List({ token }) {
         Things I'll need
       </h1>
       <div
-        class="bg-teal border-t-4 rounded-b text-midnight-green border-caribbean-green px-4 py-3 shadow-md w-full"
+        className="bg-teal border-t-4 rounded-b text-midnight-green border-caribbean-green px-4 py-3 shadow-md w-full"
         role="alert"
       >
-        <div class="flex">
-          <div class="py-1 text-caribbean-green">
+        <div className="flex">
+          <div className="py-1 text-caribbean-green">
             <svg
-              class="fill-current h-6 w-6 text-teal-500 mr-4"
+              className="fill-current h-6 w-6 text-teal-500 mr-4"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
             >
               <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
             </svg>
           </div>
-          <div class="flex items-center justify-around w-full">
-            <p class="font-bold">Here's your token</p>
-            <p class="text-sm">{token}</p>
+          <div className="flex items-center justify-around w-full">
+            <p className="font-bold">Here's your token</p>
+            <p className="text-sm">{token}</p>
           </div>
         </div>
       </div>
